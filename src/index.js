b4f61938cd3429ab0fd7a3729842a3c1c461f0db
Tim Allen <tim@noblesamurai.com>
2020-02-11 15:39:37 +1100

    Initial cut.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-export default function (input, options = {}) {
-  if (typeof input !== 'string') {
-    throw new Error('Expected input to be string');
-  }
+const AMQP = require('simple-amqplib');
+const { callbackify } = require('util');
 
-  return `${input} & ${options.postfix || 'rainbow'}`;
+async function main (amqpConfig, process, opts = {}) {
+  const amqp = new AMQP(amqpConfig);
+  const { init = [] } = opts;
+
+  // Wait for setup/init promises to resolve
+  await Promise.all([...init, amqp.connect()]);
+  amqp.consume(callbackify(process));
 }
+
+module.exports = main;
--- test/index.js
+++ test/index.js
@@ -1,3 +1,23 @@
-describe('my module', function () {
-  it('does things', function () {});
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+const consumeStub = sinon.stub().resolves();
+const amqpStub = function () {
+  return {
+    connect: sinon.stub().resolves(),
+    consume: consumeStub
+  };
+};
+const consumer = proxyquire('..', {
+  'simple-amqplib': amqpStub
+});
+describe('simple-amqplib-consumer', function () {
+  beforeEach(function () {
+    consumeStub.reset();
+  });
+  it('works', async function () {
+    await consumer({}, async => {});
+    expect(consumeStub.callCount).to.be.greaterThan(0);
+    expect(typeof consumeStub.getCall(0).args[0]).to.equal('function');
+  });
 });
